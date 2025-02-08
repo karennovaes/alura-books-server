6370066c7509a565a3657df3e717fd6e8dae1761
karennovaes <karen-nd@outlook.com>
2025-02-07 23:35:43 -0300

    Rotas de post, patch e delete

--- rotas/livro.js
+++ rotas/livro.js
@@ -3,8 +3,20 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('O Livro está disponível');
+    res.send('Olá Mundo da Alura do Brasil!');
 }
 );
+
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST');
+});
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH');
+});
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE');
+});
 
 module.exports = router;
