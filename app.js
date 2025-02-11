dd742260bd5d09f22ccb804bcf8362899f39f3c2
karennovaes <karen-nd@outlook.com>
2025-02-10 21:18:38 -0300

    Adicionando suporte a CORS para permitir requisições de outros domínios

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // ------ IMPORTAÇÕES ------
 const rotaLivro = require('./rotas/livro');
 const express = require('express'); //cria o servidor
+const cors = require('cors'); // Permite requisições de outros domínios
 
 
 // ----- DADOS DO SERVIDOR ----- 
 
 const app = express(); //cria a aplicação
 app.use(express.json()); // Define que a aplicação vai usar JSON
+app.use(cors({origin: 'http://localhost:3000'})); // Define que a aplicação aceita requisições de qualquer origem
 const port = 8000; // Porta que o servidor vai rodar
 
 
